617f487ca9c91a0a6dbc2528a55ff5d888ba7d4e
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-08-08 12:32:39 -0300

    pagina de login

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
+import logo from "../../assets/logo.png";
+import "./signin.css";
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo sistema de chamados" />
+        </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
