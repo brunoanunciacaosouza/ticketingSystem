60a20904ce33419f4ce881a2fb108c7cfa8b1f97
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-08-28 11:59:32 -0300

    criando tabela de tickets

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
+
 import Header from "../../components/Header";
+
+import "./dashboard.css";
+import { FiEdit, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
+import { Title } from "../../components/Title";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const { logout } = useContext(AuthContext);
@@ -11,8 +17,86 @@ export default function Dashboard() {
 
   return (
     <div>
-      <Header/>
-      <h1>Dashboard</h1>
+      <Header />
+
+      <div className="content">
+        <Title name="Tickets">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus color="#fff" size={25} />
+            Novo chamado
+          </Link>
+
+          <div className="container dashboard"></div>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrado em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Mercado Esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: "#999" }}>
+                    Em Aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">12/05/2022</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: "#3583f6" }}
+                  >
+                    <FiSearch color="#fff" size={25} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: "#f6a935" }}
+                  >
+                    <FiEdit color="#fff" size={25} />
+                  </button>
+                </td>
+              </tr>
+
+              <tr>
+                <td data-label="Cliente">Mercado Esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: "#999" }}>
+                    Em Aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">12/05/2022</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: "#3583f6" }}
+                  >
+                    <FiSearch color="#fff" size={25} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: "#f6a935" }}
+                  >
+                    <FiEdit color="#fff" size={25} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
+
       <button onClick={handleLogout}>Sair da conta</button>
     </div>
   );
